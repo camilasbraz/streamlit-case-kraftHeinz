c269abec3a779783cb8124fdadd6716a58d92ce3
camilasbraz <camilabraz03@gmail.com>
2023-08-31 07:58:52 -0300

    fixed requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,9 @@ st.set_page_config(page_icon=favicon_url)
 
 # Título e imagem
 col1, col2 = st.columns([1, 3])  # Distribuição de largura das colunas
-
 # Coluna 1 (imagem)
 with col1:
     st.image(imagem_url)
-
 # Coluna 2 (título)
 with col2:
     st.title("Comparação de dados das bases ADP e Workday")
